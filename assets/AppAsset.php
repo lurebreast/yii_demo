28cb039251f03683aa9ba7b0e0309a78d3f4b240
zhangguichuan <zhangguichuan@youxiduo.com>
2018-10-14 11:32:55 +0800

    assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,9 +23,11 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/hello.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+    public $jsOptions = ['position' => \yii\web\View::POS_HEAD];
 }
---
+++ web/js/hello.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Henry on 2018/10/14.
+ */
+(function(){
+    alert('hello js1');
+})(window)
